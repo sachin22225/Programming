1bd934cbf03195ea386c16a775702853c4b9c466
sachin22225 <56403571+sachin22225@users.noreply.github.com>
2019-10-23 23:59:44 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import seaborn as sns
+
+np.random.seed(2)
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import itertools
+
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+
+import pandas as pd
+train=pd.read_csv("characters-digits-train.csv")
+Y_train = train["45"]
+X_train = train.drop(labels = ["45"],axis = 1)
+X_train = X_train.values.reshape(-1,28,28,1)
+Y_train = to_categorical(Y_train, num_classes = 47)
+random_seed=2
+X_train, X_val, Y_train, Y_val = train_test_split(X_train, Y_train, test_size = 0.1, random_state=random_seed)
+model = Sequential()
+
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu', input_shape = (28,28,1)))
+model.add(Conv2D(filters = 32, kernel_size = (5,5),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2)))
+model.add(Dropout(0.25))
+
+
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(Conv2D(filters = 64, kernel_size = (3,3),padding = 'Same', 
+                 activation ='relu'))
+model.add(MaxPool2D(pool_size=(2,2), strides=(2,2)))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(256, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(128, activation = "relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation = "relu"))
+model.add(Dropout(0.5))
+
+model.add(Dense(47, activation = "softmax"))
+optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+model.compile(optimizer = optimizer , loss = "categorical_crossentropy", metrics=["accuracy"])
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=3, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+epochs = 5 # Turn epochs to 30 to get 0.9967 accuracy
+batch_size = 4
+history = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, 
+         validation_data = (X_val, Y_val), verbose = 2)
